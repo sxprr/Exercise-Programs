1bd21b16b0d8b3e3f305b6ebf6ec4ceb0cab58a2
sxprr <magalajawad@gmail.com>
2018-06-30 23:08:47 +0100

    new update June 30th
    i am a wierdo.

--- Exercise Programs/ex1.py
+++ Exercise Programs/ex1.py
@@ -1,4 +1,98 @@
-import os
+# This is my first programming exercise, a password generator
+# This is what my approach was to programming.
+
+# take a look and give me feedback.
+
+import os 
+import random 
+import string
 
 UserPswrdLvl = input("Choose password strength by corresponding letter - [Short (S)]-[Medium (M)]-[Long (L)]")
 UserAlphNum = input("Alphanumeric? (Y/N)?")
+
+# function to generate small password string
+
+def PassWordGen_S():
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+
+    return 0 
+
+# function to generate medium password string
+def PassWordGen_M():
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+
+    
+    return 0 
+
+# function to generate medium password string
+def PasswordGen_L():
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+    random.choice(string.ascii_letters)
+
+    return 0 
+
+print(PassWordGen_S)
+print(PassWordGen_M)
+print(PasswordGen_L)
+
